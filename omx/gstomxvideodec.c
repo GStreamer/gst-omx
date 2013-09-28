GStreamer/gstreamer

omx: don't use the 'z' modifier to print size_t
gcc will warn in some cases even if the size of the type
is exactly that of size_t on the platform.

https://bugzilla.gnome.org/show_bug.cgi?id=699008

omx/gstomxvideodec.c
@@ -3015,8 +3015,8 @@ too_large_codec_data:
   {
     gst_video_codec_frame_unref (frame);
     GST_ELEMENT_ERROR (self, STREAM, FORMAT, (NULL),
-        ("codec_data larger than supported by OpenMAX port (%zu > %u)",
-            gst_buffer_get_size (codec_data),
+        ("codec_data larger than supported by OpenMAX port "
+            "(%" G_GSIZE_FORMAT " > %u)", gst_buffer_get_size (codec_data),
             (guint) self->dec_in_port->port_def.nBufferSize));
     return GST_FLOW_ERROR;
   }

GStreamer/gst-omx

Conflicts:
	omx/gstomxvideodec.c

omx/gstomxvideodec.c
@@ -2399,8 +2399,8 @@ too_large_codec_data:
   {
     gst_video_codec_frame_unref (frame);
     GST_ELEMENT_ERROR (self, STREAM, FORMAT, (NULL),
-        ("codec_data larger than supported by OpenMAX port (%zu > %zu)",
-            GST_BUFFER_SIZE (codec_data),
+        ("codec_data larger than supported by OpenMAX port "
+            "(%" G_GSIZE_FORMAT " > %u)", GST_BUFFER_SIZE (codec_data),
             (guint) self->dec_in_port->port_def.nBufferSize));
     return GST_FLOW_ERROR;
   }